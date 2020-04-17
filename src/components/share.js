21900738f4984f896340ed3e940262a21b9e5240
amrancz <adam@amran.cz>
2020-04-17 08:55:27 +0200

    Copy link to share

--- src/components/share.js
+++ src/components/share.js
@@ -1,25 +1,63 @@
-import React, { useState }  from "react"
+import React  from "react"
 import style from "./share.module.css"
-import { FiCopy, FiLinkedin } from "react-icons/fi";
-import { FiTwitter } from "react-icons/fi";
+import { FiCopy, FiTwitter } from "react-icons/fi";
 import { IconContext } from "react-icons";
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const url = window.location.href
+class Tooltip extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tooltipText: 'Copy link',
+        };
+    }
 
-const Tooltip = () => {
-    const [tooltipText, setText] = useState("Click here");
-
-    return <p onClick={() => setText("New title")}className={style.tooltip}>{tooltipText}</p>
+    render () {
+        return (
+            <p tooltipText={this.props.tooltipText} className={style.tooltip}>{this.props.tooltipText}</p>
+        )
+    }
 }
 
-const Share = () => (
-    <div className={style.share}>
-        <span>Share</span>
-        <CopyToClipboard text={url}><button><IconContext.Provider value={{ color: "var(--darkGrey)", className: "global-class-name", size: "2.4rem" }}><FiCopy /></IconContext.Provider><Tooltip tooltipText="Copy link"></Tooltip></button></CopyToClipboard>
-        <button><IconContext.Provider value={{ color: "#1DA1F2", className: "global-class-name", size: "2.4rem" }}><FiTwitter /></IconContext.Provider><p className={style.tooltip}>Share on Twitter</p></button>
-        <button><IconContext.Provider value={{ color: "#007BB5", className: "global-class-name", size: "2.4rem" }}><FiLinkedin /></IconContext.Provider><p className={style.tooltip}>Share on LinkedIn</p></button>
-    </div>
-)
+/* 
+const Tooltip = (tooltipText) => {
+    return <p className={style.tooltip}>{tooltipText}</p>
+} */
+
+class Share extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tooltipText: 'Copy link',
+        };
+        this.changeText = this.changeText.bind(this);
+        this.resetText = this.resetText.bind(this)
+    }
+
+    changeText(){
+        this.setState({
+            tooltipText: 'Copied!'
+        })
+    }
+
+    resetText(){
+        this.setState({
+            tooltipText: 'Copy link!'
+        })
+    }
+
+    componentDidMount() {
+        this.url = window.location.href
+    }
+
+    render() {
+        return (
+        <div className={style.share}>
+            <span>Share this tool:</span>
+            <CopyToClipboard text={this.url}><button onMouseEnter={this.resetText} onClick={this.changeText}><IconContext.Provider value={{ color: "var(--darkGrey)", className: "global-class-name", size: "2.4rem" }}><FiCopy /></IconContext.Provider><Tooltip tooltipText={this.state.tooltipText}></Tooltip></button></CopyToClipboard>
+            <button><IconContext.Provider value={{ color: "#1DA1F2", className: "global-class-name", size: "2.4rem" }}><FiTwitter /></IconContext.Provider><p className={style.tooltip}>Share on Twitter</p></button>
+        </div>
+    )}
+}
   
 export default Share
