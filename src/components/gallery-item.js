8b33a358743597fb3228970b5d75bd9bd2cc57f3
amrancz <adam@amran.cz>
2020-04-20 19:09:14 +0200

    Responsive styling

--- src/components/gallery-item.js
+++ src/components/gallery-item.js
@@ -9,8 +9,8 @@ const GalleryItem = ({data}) => {
     <Link to={`/${data.elements.url_slug.value}`} key={data.id} className={style.item}>
         <div>
             <img src={`${data.elements.icon.value[0].url}`} alt=""></img>
-            <h2>{data.elements.name.value}</h2>
-            <Tag categoryStyle={categoryTag} category={data.elements.category.value[0].name.toUpperCase()}></Tag>
+              <h2>{data.elements.name.value}</h2>
+              <Tag categoryStyle={categoryTag} category={data.elements.category.value[0].name.toUpperCase()}></Tag>
             <p>{data.elements.when_useful.value}</p>
         </div>
     </Link>
